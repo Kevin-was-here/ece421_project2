3ac48bb747b6037d0581ced5156260d8584c4416
kevin-was-here <code --wait>
2024-03-14 10:14:55 -0600

    new skeleton

---
+++ trees/bench/benchmark.rs
@@ -0,0 +1,5 @@
+// for tree_size in (10,000, 40,000, 70,000, 100,000, 130,000) do:
+// Start by creating an empty tree.
+// Values with tree_size are inserted into the tree.
+// A search is conducted for the (tree_size/10) lowest values.
+// end
